7816d9d54d228f6ad208327692ba128f49b78d66
bakamarv <bakamarv@gmail.com>
2020-01-18 17:14:37 +0300

    Исправление замечаний (пусые строки, пробелы, имена переменных)

--- lesson_1/ConditionalStatement.java
+++ lesson_1/ConditionalStatement.java
@@ -2,46 +2,32 @@ import java.util.Scanner;
 
 public class ConditionalStatement {
     public static void main(String[] args) {
-        int Age;
-        char  Pol;
-        double Rost;
-        char Name;
-
+        int age;
+        char  pol;
+        double rost;
+        char name;
         System.out.print (" Введите ваш возраст ");
         Scanner inputFigure = new Scanner (System.in);
-        Age = inputFigure.nextInt ();
-
-
-        if (Age > 20)
+        age = inputFigure.nextInt ();
+        if (age > 20)
         {
-            System.out.println("Ваш возраст больше 20= "+ Age);
+            System.out.println("Ваш возраст больше 20= "+ age);
         }
-
-
         System.out.println ("Введите ваш пол M или W  ");
-
         inputFigure.nextLine ();
-            Pol = inputFigure.nextLine().charAt(0);
-
-        if (Pol == 'M') {System.out.println("Мужской");}
-        if (Pol != 'M') {System.out.println("Не мужской");}
-
+        pol = inputFigure.nextLine().charAt(0);
+        if (pol == 'M') {System.out.println("Мужской");}
+        if (pol != 'M') {System.out.println("Не мужской");}
         System.out.print ("Введите свой рост (в метрах) ");
-        Rost =  inputFigure.nextDouble ();
-
-        if (Rost < 1.8) {System.out.println("Ваш рост менее 1.8");}
+        rost =  inputFigure.nextDouble ();
+        if (rost < 1.8) {System.out.println("Ваш рост менее 1.8");}
         else {System.out.println("Ваш рост более 1.8");}
-
         System.out.print ("Введите первую букву вашего имени (в транскрибции)");
                 inputFigure.nextLine ();
-                Name = inputFigure.nextLine().charAt(0);
-
-
-        if (Name == 'M') {System.out.println("Ваше имя начинается с M");}
+                name = inputFigure.nextLine().charAt(0);
+        if (name == 'M') {System.out.println("Ваше имя начинается с M");}
         else
-            if (Name == 'I') {System.out.println("Ваше имя начинается с I ");}
+            if (name == 'I') {System.out.println("Ваше имя начинается с I ");}
             else {System.out.println("Ваше имя начинается ни с M и не с I");}
-
     }
-
 }
