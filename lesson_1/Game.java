8620351abcd1d126597577712d1ff4ef06caff8f
bakamarv <bakamarv@gmail.com>
2020-01-18 17:30:43 +0300

    Исправление замечаний (пусые строки, пробелы, имена переменных)

--- lesson_1/Game.java
+++ lesson_1/Game.java
@@ -1,24 +1,18 @@
 import java.util.Random;
 import java.util.Scanner;
 
-
-public class Game {	
+public class Game {
     public static void main(String[] args) {
-
         int min = 0;
         int max = 100;
         int i = -1;
-
         int number;
-
         final Random rnd = new Random();
         number = min + rnd.nextInt(max - min + 1);
-
         System.out.println("Угадайте число от 0 до 100 ");
         Scanner inputFigure = new Scanner (System.in);
-        
         while (i != number){
-            i = inputFigure.nextInt ();
+            i = inputFigure.nextInt();
             if (number > i){
                 System.out.println("Введенное вами число меньше того, что загадал компьютер");
             };
