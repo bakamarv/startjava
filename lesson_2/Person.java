30adbf70bbad9882e3c37584ebffe684f716ee31
bakamarv <bakamarv@gmail.com>
2020-01-29 16:49:40 +0300

    Урок 2

---
+++ lesson_2/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+
+    char gender = 'M';
+    String fullName = "Ivan Petrov";
+    float height = 1.85;
+    float wheight = 70;
+    float age = 30;
+
+    void sit() {
+
+    }
+
+    void run() {
+
+    }
+
+    void go() {
+
+    }
+
+    void talk(){
+
+    }
+
+    void studyJava() {
+
+    }
+
+}
