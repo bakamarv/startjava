c8b1e30b0fcbd5346cffd315dd500dda7115b426
bakamarv <bakamarv@gmail.com>
2020-01-30 17:18:12 +0300

    Доюавлены геттеры и сеттеры в классы Wolf и WolfTest

--- lesson_2/Wolf.java
+++ lesson_2/Wolf.java
@@ -1,24 +1,73 @@
 public class Wolf {
 
-    char gender;
-    String name;
-    float weigth;
-    float age;
-    String color;
+    private char gender;
+    private String name;
+    private float weigth;
+    private float age;
+    private String color;
 
     void go() {
 
     }
+
     void sit() {
 
     }
+
     void run() {
 
     }
+
     void howl() {
         System.out.println("Owooooooo");
     }
+
     void hunt() {
 
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public float getWeigth() {
+        return weigth;
+    }
+
+    public void setWeigth(float weigth) {
+        this.weigth = weigth;
+    }
+
+    public float getAge() {
+        return age;
+    }
+
+    public void setAge(float age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
 }
+
--- lesson_2/WolfTest.java
+++ lesson_2/WolfTest.java
@@ -3,15 +3,26 @@ public class WolfTest {
     public static void main(String[] args) {
 
         Wolf myWolf = new Wolf();
-        myWolf.name = "Misha";
+        /*myWolf.name = "Misha";
         myWolf.age = 7;
         myWolf.weigth = 22;
-        myWolf.color = "white";
+        myWolf.color = "white";*/
+
+        myWolf.setName("Misha");
+        myWolf.setAge(7);
+        myWolf.setWeigth(22);
+        myWolf.setColor("white");
+
+        System.out.println(myWolf.getName());
+        System.out.println(myWolf.getAge());
+        System.out.println(myWolf.getWeigth());
+        System.out.println(myWolf.getColor());
 
         myWolf.go();
         myWolf.sit();
         myWolf.run();
         myWolf.hunt();
         myWolf.howl();
+
     }
 }
