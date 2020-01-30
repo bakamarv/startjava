aed1e3d0a39fb0208550f8b120e384e59171a94a
bakamarv <bakamarv@gmail.com>
2020-01-31 00:41:27 +0300

    Добавлен Jaeger из урока

---
+++ lesson_2/Jaeger.java
@@ -0,0 +1,27 @@
+public class Jaeger {
+
+    String modelName = "Bracer Phoenix";
+    String mark = "Mark-5";
+    String origin = "USA";
+    float height = 70.7;
+    float weihht = 2.1;
+    int speed = 3;
+    int strength = 8;
+    int armot = 9;
+
+    boolean drift() {
+
+    }
+
+    voud move() {
+
+    }
+
+    String scanKaiju() {
+
+    }
+
+    void useVortexCannon() {
+
+    }
+}
